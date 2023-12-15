bf2052fa716d5c832cbfdde2cea3dfc6fc1629eb
uNastyy_ <bouissouromain82@gmail.com>
2023-12-15 11:01:06 +0100

    update supp

--- PHP/supprimer.php
+++ PHP/supprimer.php
@@ -14,23 +14,40 @@
   </head>
   <body>
     <?php
-        require("fonctions.php");
-        $BDD =BDD::getInstanceBDD();
-        $result = $BDD->select("usager");
+        require("BDDusager.php");
+        $BDD = new BDDusager();
+        $result = $BDD->select();
+/*
+        if( isset($_POST['supp'])  ){
+            // ceci est la requete que je veux executer après le clique 
+            try{
+               $req = $database->query('SELECT nom FROM teams WHERE id = 1');
+               $team1 = $req->fetch();
+            }catch(Exception $e){
+               echo "Erreur " . $e->getMessage();
+            }
+               $nom = !empty($team1['nom']) ? $team1['nom']: "";
+        }
+*/
         echo "<table>";
         echo "<tr>";
         echo "<th>Nom</th>";
         echo "<th>Prenom</th>";
-        echo "<th>Civilite</th>";
+        echo "<th>Civilité</th>";
+        echo "<th>Choix</th>";
         echo "</tr>";
         while($row = $result->fetch()) {
             echo "<tr>";
             echo "<td>" . $row["Nom"] . "</td>";
             echo "<td>" . $row["Prenom"] . "</td>";
             echo "<td>" . $row["Civilite"] . "</td>";
+            echo "<td> 
+                <button id='supp' name='supp'><img src='../IMAGES/icons8-trash-50.png'/> </button>
+            </td>";
             echo "</tr>";
         }
         echo "</table>";
+        
     ?>
   </body>
 </html>
