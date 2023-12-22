01f6557ca63719e941a9fac88591665bfdb99bd1
uNastyy_ <bouissouromain82@gmail.com>
2023-12-22 11:04:23 +0100

    update css

--- PHP/supprimer.php
+++ PHP/supprimer.php
@@ -12,34 +12,40 @@
     </style>
   </head>
   <body>
-  <div class="choice-box">
-    
-  <h2>Page de suppression de médecin</h2>
-    
-        
-         <table>
-         <tr>
-         <th>ID</th>
-         <th>Nom</th>
-         <th>Prenom</th>
-         <th>Civilité</th>
-         <th>Choix</th>
-         </tr>
-        <?php
+    <div class="choice-box">
+      <h2>Page de suppression de médecin</h2>
+      <table>
+      <tr>
+        <th>ID</th>
+        <th>Nom</th>
+        <th>Prenom</th>
+        <th>Civilité</th>
+        <th>Choix</th>
+      </tr>
+      
+      <?php
         require("BDDmedecin.php");
         $BDD = new BDDmedecin();
         $records = $BDD->select();
-
         while($row = $records->fetch()) {
-            $recordID = $row["ID"];?>
-            <tr>
+          $recordID = $row["ID"];?>
+          <tr>
             <td><?php echo $recordID;?></td>
             <td><?php echo $row["Nom"];?></td>
             <td><?php echo$row["Prenom"];?></td>
-            <td><?php echo$row["Civilite"];?></td><td><a href='delete-script.php?recordID=<?php echo $recordID?>'>Delete</a> </td>
-            </tr>;
-        <?php }
-        ?>
+            <td><?php echo$row["Civilite"];?></td>
+            <td>
+              <a href='delete-script.php?recordID=<?php echo $recordID?>' class="delete-icon">
+              <img src="../IMAGES/icons8-trash-50.png"></a> 
+            </td>
+          </tr>;
+        <?php } ?>  
+        </table>
+        <form>
+          <a href="index.php" class="choice-button" id="retour">
+            Retour 
+          </a>
+        </form>
     </div>
   </body>
 </html>
