95cc25c635e42a4cbcce824265f2062f935fdaa6
uNastyy_ <119569179+uNastyy@users.noreply.github.com>
2024-01-03 21:33:40 +0100

    ptit oubli

--- PHP/medecin/modifier.php
+++ PHP/medecin/modifier.php
@@ -6,37 +6,12 @@
         <link rel="stylesheet" href="../../CSS/style.css">
     </head>
     <header>
-        <nav>
-            <ul>
-                <li class="deroulant"><a href="#">Médecin &ensp;</a>
-                <ul class="sous">
-                    <li><a href="supprimer.php">Supprimer un médecin</a></li>
-                    <li><a href="ajouter.php">Ajouter un médecin</a></li>
-                    <li><a href="modifier.php">Modifier un médecin</a></li>
-                </ul>
-                </li>
-                <li class="deroulant"><a href="#">Usager &ensp;</a>
-                <ul class="sous">
-                <li><a href="supprimer.php">Supprimer un usager</a></li>
-                    <li><a href="ajouter.php">Ajouter un usager</a></li>
-                    <li><a href="modifier.php">Modifier un usager</a></li>
-                </ul>
-                </li>
-                <li class="deroulant"><a href="#">Consultations &ensp;</a>
-                <ul class="sous">
-                <li><a href="supprimer.php">Supprimer</a></li>
-                    <li><a href="ajouter.php">Ajouter</a></li>
-                    <li><a href="modifier.php">Modifier</a></li>
-                </ul>
-                </li>
-                <li><a href="#">A propos</a></li>
-            </ul>
-        </nav>
+  <?php include "../../HTML/header.php"; ?>
     </header>
 
     <body>
-        <h2>Page de suppression</h2>
         <div class="scrollable-div choice-box">
+        <h2>Page de suppression</h2>
         <form action="#" method="post" class="formulaire">
             <label>Nom :</label>
             <input name="nom" id="nom" type="text" /></p>
