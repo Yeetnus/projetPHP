5737d04c647456475bbce51e125fdf93f7b37b3c
uNastyy_ <119569179+uNastyy@users.noreply.github.com>
2024-01-14 18:51:43 +0100

    joli code

--- choix.php
+++ choix.php
@@ -12,49 +12,49 @@
 <body>
   <div class="global-box">
     <h2>Où souhaitez-vous aller ?</h2>
-  <div class="container">
-        <div class="box">
-          <a href="PHP/medecin/supprimer.php" class="choice-button">
-            Supprimer un médecin
-          </a><br>
-          <a href="PHP/medecin/modifier.php" class="choice-button">
-            Modifier un médecin
-          </a><br>
-          <a href="PHP/medecin/ajouter.php" class="choice-button">
-            Ajouter un médecin
-          </a>
-        </div>
-        <div class="box">
-          <a href="PHP/usager/supprimer.php" class="choice-button">
-            Supprimer un usager
-          </a><br>
-          <a href="PHP/usager/modifier.php" class="choice-button">
-            Modifier un usager
-          </a><br>
-          <a href="PHP/usager/ajouter.php" class="choice-button">
-            Ajouter un usager
-          </a>
-        </div>
-        <div class="box">
-          <a href="PHP/rdv/supprimer.php" class="choice-button">
-            Supprimer un rendez-vous
-          </a><br>
-          <a href="PHP/rdv/modifier.php" class="choice-button">
-            Modifier un rendez-vous
-          </a><br>
-          <a href="PHP/rdv/ajouter.php" class="choice-button">
-            Ajouter un rendez-vous
-          </a>
-        </div>
-        <div class="box">
-          <a href="PHP/stats.php" class="choice-button">
-            Statistiques
-          </a>
-        </div>
-  </div>
-  <form action="PHP/logout.php" method="post" >
-    <input id="logout" type="submit" value="Logout">
-</form>
+    <div class="container">
+      <div class="box">
+        <a href="PHP/medecin/supprimer.php" class="choice-button">
+          Supprimer un médecin
+        </a><br>
+        <a href="PHP/medecin/modifier.php" class="choice-button">
+          Modifier un médecin
+        </a><br>
+        <a href="PHP/medecin/ajouter.php" class="choice-button">
+          Ajouter un médecin
+        </a>
+      </div>
+      <div class="box">
+        <a href="PHP/usager/supprimer.php" class="choice-button">
+          Supprimer un usager
+        </a><br>
+        <a href="PHP/usager/modifier.php" class="choice-button">
+          Modifier un usager
+        </a><br>
+        <a href="PHP/usager/ajouter.php" class="choice-button">
+          Ajouter un usager
+        </a>
+      </div>
+      <div class="box">
+        <a href="PHP/rdv/supprimer.php" class="choice-button">
+          Supprimer un rendez-vous
+        </a><br>
+        <a href="PHP/rdv/modifier.php" class="choice-button">
+          Modifier un rendez-vous
+        </a><br>
+        <a href="PHP/rdv/ajouter.php" class="choice-button">
+          Ajouter un rendez-vous
+        </a>
+      </div>
+      <div class="box">
+        <a href="PHP/stats.php" class="choice-button">
+          Statistiques
+        </a>
+      </div>
+    </div>
+    <form action="PHP/logout.php" method="post">
+      <input id="logout" type="submit" value="Logout">
+    </form>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="CSS/style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="CSS/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="IMAGES/logo_cabinet.png">
-    <title>Connexion</title>
-  </head>
-  <body>
-    <div class="scrollable-div login-box" >
-      <h2>Page de connexion</h2>
-      <form action="login.php" method="post" class="formulaire">
-        <div class="user-box">
-          <input type="text" id="username" name="username" required>
-          <label>Username</label>
-        </div>
-        <div class="user-box">
-          <input type="password" id="password" name="password" required>
-          <label>Password</label>
-        </div>
-        <?php if (isset($_GET['error'])) : ?>
+  <title>Connexion</title>
+</head>
+
+<body>
+  <div class="scrollable-div login-box">
+    <h2>Page de connexion</h2>
+    <form action="login.php" method="post" class="formulaire">
+      <div class="user-box">
+        <input type="text" id="username" name="username" required>
+        <label>Username</label>
+      </div>
+      <div class="user-box">
+        <input type="password" id="password" name="password" required>
+        <label>Password</label>
+      </div>
+      <?php if (isset($_GET['error'])): ?>
         <p class="error-message" style="color: red;">Le nom d'utilisateur ou le mot de passe est incorrect.</p>
       <?php endif; ?>
-        <input type="submit" value="Login" class="choice-button retour  ">
-      </form>
-      
-      </div>
-      
-  </body>
+      <input type="submit" value="Login" class="choice-button retour  ">
+    </form>
+
+  </div>
+
+</body>
+
 </html>
