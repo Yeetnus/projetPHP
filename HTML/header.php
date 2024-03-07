26b9b09a66e94aa1067928b34ee69ea2e37fc703
uNastyy_ <bouissouromain82@gmail.com>
2024-03-07 08:24:34 +0100

    php

--- HTML/header.php
+++ HTML/header.php
@@ -15,29 +15,29 @@
         <ul class="<vertical-menu">
             <li>Médecin
                 <ul>
-                    <li><a href="/php/medecin/supprimer.php">Supprimer un médecin</a></li>
-                    <li><a href="/php/medecin/ajouter.php">Ajouter un médecin</a></li>
-                    <li><a href="/php/medecin/modifier.php">Modifier un médecin</a></li>
+                    <li><a href="/PHP/medecin/supprimer.php">Supprimer un médecin</a></li>
+                    <li><a href="/PHP/medecin/ajouter.php">Ajouter un médecin</a></li>
+                    <li><a href="/PHP/medecin/modifier.php">Modifier un médecin</a></li>
                 </ul>
             </li>
             <hr />
             <li>Usager
                 <ul>
-                    <li><a href="/php/usager/supprimer.php">Supprimer un usager</a></li>
-                    <li><a href="/php/usager/ajouter.php">Ajouter un usager</a></li>
-                    <li><a href="/php/usager/modifier.php">Modifier un usager</a></li>
+                    <li><a href="/PHP/usager/supprimer.php">Supprimer un usager</a></li>
+                    <li><a href="/PHP/usager/ajouter.php">Ajouter un usager</a></li>
+                    <li><a href="/PHP/usager/modifier.php">Modifier un usager</a></li>
                 </ul>
             </li>
             <hr />
             <li>Consultations
                 <ul>
-                    <li><a href="/php/rdv/supprimer.php">Supprimer une consultation</a></li>
-                    <li><a href="/php/rdv/ajouter.php">Ajouter une consultation</a></li>
-                    <li><a href="/php/rdv/modifier.php">Modifier une consultation</a></li>
+                    <li><a href="/PHP/rdv/supprimer.php">Supprimer une consultation</a></li>
+                    <li><a href="/PHP/rdv/ajouter.php">Ajouter une consultation</a></li>
+                    <li><a href="/PHP/rdv/modifier.php">Modifier une consultation</a></li>
                 </ul>
             </li>
             <hr />
-            <li><a href="/php/stats.php">Statistiques</a></li>
+            <li><a href="/PHP/stats.php">Statistiques</a></li>
         </ul>
     </nav>
 
