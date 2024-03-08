8bc4faddb3ffd603ba04ea62d7b9d99f14385945
Yeetnus <yanis.croguennoc@etu.iut-tlse3.Fr>
2024-03-08 10:17:29 +0100

    bon dossiers

---
+++ API FUNC/BDD.php
@@ -0,0 +1,32 @@
+<?php
+class BDD
+{
+    private static $_instance = null;
+    private $linkpdo;
+
+    private function __construct()
+    {
+        $this->linkpdo = new PDO(
+            "mysql:host=db_project;dbname=php_project;charset=utf8",
+            "test",
+            "test"
+        );
+    }
+
+    public static function getInstanceBDD()
+    {
+
+        if (is_null(self::$_instance)) {
+            self::$_instance = new BDD();
+        }
+
+        return self::$_instance;
+    }
+
+    public function getBDD()
+    {
+        return $this->linkpdo;
+    }
+
+}
+?>
