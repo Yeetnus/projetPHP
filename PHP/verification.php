88b65dc0ccf8614a0d5c727d79482ceda60e656f
uNastyy_ <bouissouromain82@gmail.com>
2024-03-21 09:05:34 +0100

    change un truc

--- PHP/verification.php
+++ PHP/verification.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 if (!isset($_SESSION['session'])) {
-    header('Location: /projet/projetPHP/index.php');
+    header('Location: /projet/projetPHP/index.html');
     exit();
 }
