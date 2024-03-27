25aa8869ddbdaf001c44c3e7acaf056a4c54bba8
uNastyy_ <bouissouromain82@gmail.com>
2024-03-27 14:09:56 +0100

    identation

--- API_METHOD/medecins.php
+++ API_METHOD/medecins.php
@@ -96,16 +96,16 @@ case "GET" :
     }
 
     function deliver_response($status_code, $status_message, $data=null){
-    http_response_code($status_code); //Utilise un message standardisé en fonction du code HTTP
-    //header("HTTP/1.1 $status_code $status_message"); //Permet de personnaliser le message associé au code HTTP
-    header("Content-Type:application/json; charset=utf-8");//Indique au client le format de la réponse
-    $response['status_code'] = $status_code;
-    $response['status_message'] = $status_message;
-    $response['data'] = $data;
-    /// Mapping de la réponse au format JSON
-    $json_response = json_encode($response);
-    if($json_response===false)
-     die('json encode ERROR : '.json_last_error_msg());
-    /// Affichage de la réponse (Retourné au client)
-    echo $json_response;
+        http_response_code($status_code); //Utilise un message standardisé en fonction du code HTTP
+        //header("HTTP/1.1 $status_code $status_message"); //Permet de personnaliser le message associé au code HTTP
+        header("Content-Type:application/json; charset=utf-8");//Indique au client le format de la réponse
+        $response['status_code'] = $status_code;
+        $response['status_message'] = $status_message;
+        $response['data'] = $data;
+        /// Mapping de la réponse au format JSON
+        $json_response = json_encode($response);
+        if($json_response===false)
+        die('json encode ERROR : '.json_last_error_msg());
+        /// Affichage de la réponse (Retourné au client)
+        echo $json_response;
 }
