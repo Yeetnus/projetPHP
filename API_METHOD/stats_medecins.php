88844fad191e50e95c7350728fc07d18a2356069
Yeetnus <yanis.croguennoc@etu.iut-tlse3.Fr>
2024-03-28 09:53:00 +0100

    test api stats

---
+++ API_METHOD/stats_medecins.php
@@ -0,0 +1,34 @@
+<?php
+require('jwt_utils.php');
+require('../FUNC/functions_stats.php');
+$popo = new functions_stats();
+
+$http_method = $_SERVER['REQUEST_METHOD'];
+switch ($http_method){
+case "GET" :
+    $jwt=get_bearer_token();
+    if(is_jwt_valid($jwt,'secret')){
+        $matchingData=$popo->getStatsMedecins();
+        deliver_response(200,"tout s'est bien passé",$matchingData);
+    }else{
+        deliver_response(400, 'Votre token n\'est pas bon');
+    }
+    break;
+default:
+    deliver_response(405, 'Method Not Allowed');
+    break;
+}
+function deliver_response($status_code, $status_message, $data=null){
+    http_response_code($status_code); //Utilise un message standardisé en fonction du code HTTP
+    //header("HTTP/1.1 $status_code $status_message"); //Permet de personnaliser le message associé au code HTTP
+    header("Content-Type:application/json; charset=utf-8");//Indique au client le format de la réponse
+    $response['status_code'] = $status_code;
+    $response['status_message'] = $status_message;
+    $response['data'] = $data;
+    /// Mapping de la réponse au format JSON
+    $json_response = json_encode($response);
+    if($json_response===false)
+     die('json encode ERROR : '.json_last_error_msg());
+    /// Affichage de la réponse (Retourné au client)
+    echo $json_response;
+}
--- FUNC/functions_stats.php
+++ FUNC/functions_stats.php
@@ -9,109 +9,20 @@ class functions_stats
         $this->BDD = BDD::getInstanceBDD();
     }
 
-    public function getMoins25H()
+    public function getStatsUsager()
     {
-        $sql = "SELECT * FROM usager";
+        $sql = "SELECT civilite, CASE 
+            WHEN TIMESTAMPDIFF(YEAR, date_nais, CURDATE()) < 25 THEN 'Moins de 25 ans'
+            WHEN TIMESTAMPDIFF(YEAR, date_nais, CURDATE()) BETWEEN 25 AND 50 THEN 'Entre 25 et 50 ans'
+            ELSE 'Plus de 50 ans' 
+            END AS age_group, COUNT(*) AS count
+            FROM usager
+            GROUP BY civilite, age_group";
         $records = $this->BDD->getBDD()->query($sql)->fetch(PDO::FETCH_ASSOC);
-        $result = 0;
-        $today = date('Y-m-d');
-        while ($row = $records->fetch()) {
-            $diff = date_diff(date_create($row["DateNaissance"]), date_create($today));
-            $age = $diff->format('%y');
-
-            if ($row["Sexe"] == "M" && $age < 25) {
-                $result++;
-            }
-        }
-        return $result;
+        return $records;
     }
 
-    public function getEntre25Et50H()
-    {
-        $sql = "SELECT * FROM usager";
-        $records = $this->BDD->getBDD()->query($sql)->fetch(PDO::FETCH_ASSOC);
-        $result = 0;
-        $today = date('Y-m-d');
-        while ($row = $records->fetch()) {
-            $diff = date_diff(date_create($row["DateNaissance"]), date_create($today));
-            $age = $diff->format('%y');
-
-            if ($row["Sexe"] == "M" && $age >= 25 && $age <= 50) {
-                $result++;
-            }
-        }
-        return $result;
-    }
-
-    public function getPlus50H()
-    {
-        $sql = "SELECT *  FROM usager";
-        $records = $this->BDD->getBDD()->query($sql)->fetch(PDO::FETCH_ASSOC);
-        $result = 0;
-        $today = date('Y-m-d');
-        while ($row = $records->fetch()) {
-            $diff = date_diff(date_create($row["DateNaissance"]), date_create($today));
-            $age = $diff->format('%y');
-
-            if ($row["Sexe"] == "M" && $age > 50) {
-                $result++;
-            }
-        }
-        return $result;
-    }
-
-    public function getMoins25F()
-    {
-        $sql = "SELECT *  FROM usager";
-        $records = $this->BDD->getBDD()->query($sql)->fetch(PDO::FETCH_ASSOC);
-        $result = 0;
-        $today = date('Y-m-d');
-        while ($row = $records->fetch()) {
-            $diff = date_diff(date_create($row["DateNaissance"]), date_create($today));
-            $age = $diff->format('%y');
-
-            if ($row["Sexe"] == "F" && $age < 25) {
-                $result++;
-            }
-        }
-        return $result;
-    }
-
-    public function getEntre25Et50F()
-    {
-        $sql = "SELECT * FROM usager";
-        $records = $this->BDD->getBDD()->query($sql)->fetch(PDO::FETCH_ASSOC);
-        $result = 0;
-        $today = date('Y-m-d');
-        while ($row = $records->fetch()) {
-            $diff = date_diff(date_create($row["DateNaissance"]), date_create($today));
-            $age = $diff->format('%y');
-
-            if ($row["Sexe"] == "F" && $age >= 25 && $age <= 50) {
-                $result++;
-            }
-        }
-        return $result;
-    }
-
-    public function getPlus50F()
-    {
-        $sql = "SELECT *  FROM usager";
-        $records = $this->BDD->getBDD()->query($sql)->fetch(PDO::FETCH_ASSOC);
-        $result = 0;
-        $today = date('Y-m-d');
-        while ($row = $records->fetch()) {
-            $diff = date_diff(date_create($row["DateNaissance"]), date_create($today));
-            $age = $diff->format('%y');
-
-            if ($row["Sexe"] == "F" && $age > 50) {
-                $result++;
-            }
-        }
-        return $result;
-    }
-
-    public function getAllHeures()
+    public function getStatsMedecins()
     {
         $sql = "SELECT m.Nom, m.Prenom, SUM(r.Duree_RDV)/60 as Total_Duree FROM medecin m, rendezvous r WHERE m.ID = r.MedID GROUP BY m.ID";
         $result = $this->BDD->getBDD()->query($sql)->fetch(PDO::FETCH_ASSOC);
